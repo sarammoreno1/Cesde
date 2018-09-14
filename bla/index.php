59c94cd559072698a8243ad0853b3845f10380d1
Sara Mejía Moreno <charis.0813@gmail.com>
2018-09-14 14:34:41 -0500

    Hasta_Estructurasdecontrol
    Estructuras de control

--- bla/index.php
+++ bla/index.php
@@ -10,8 +10,49 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <h1>
         <?php
-        // put your code here
+        //Esto e sun comentario
+        $PrimeraVariable = "5";
+        $SegundaVariable = "5";
+        $lenguaje = "Javascript";    
+        
+            echo "<br>";            
+            echo $PrimeraVariable*$SegundaVariable."<br>"."<br>";
+            
+            //Estructuras de control
+            
+            if ($PrimeraVariable < 1 && $SegundaVariable < 2 == 5)
+            {
+                echo 'Soy menos';                
+            }
+            else {
+                echo 'Soy mayor';
+            }
+            
+            Switch($lenguaje){
+                case "Php":
+                    echo 'Php';
+                    break;
+                case "Javascript":
+                    echo 'Javascript';
+                    break;
+                case "Html":
+                    echo 'Html';
+                    break;
+            }
+            
+            
+            
         ?>
+        </h1>
+        <p>
+            <h2>
+                <?php
+                    echo 'Esta es otra sección';
+                ?>
+            </h2>
+            
+        </p>
     </body>
 </html>
